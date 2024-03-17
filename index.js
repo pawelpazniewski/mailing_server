7aca7aa0926b6dd99bf44cd0c0213a94d4ff59a5
Pawel Pazniewski <pawelpazniewski@hotmail.com>
2024-03-17 23:32:10 +0100

    Added cors sites

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
-app.use(cors({ origin: 'http://127.0.0.1:5500' }));
+app.use(cors({ origin: ['https://dev.variotech.pl','https://variotech.pl'] }));
 
 app.post('/send', async (req, res) => {
     const formData = req.body;
