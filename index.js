a83709061d7f364d0f28e97bcb48b7b8dcf3d99c
Pawel Pazniewski <pawelpazniewski@hotmail.com>
2024-04-18 13:18:18 +0200

    added ssl

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors'; 
+import https from 'https';
+import fs from 'fs';
 import sendMail from './controller/sendmail.js';
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(cors({ origin: ['https://dev.variotech.pl','https://variotech.pl'] }));
@@ -31,6 +33,20 @@ app.post('/send', async (req, res) => {
     }
 });
 
-app.listen(PORT, () => {
+// Wczytanie klucza prywatnego i certyfikatu SSL/TLS
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/vt-mail.pazniewski.com/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/vt-mail.pazniewski.com/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/vt-mail.pazniewski.com/chain.pem', 'utf8');
+
+const credentials = {
+    key: privateKey,
+    cert: certificate,
+    ca: ca
+};
+
+// Stworzenie serwera HTTPS
+const httpsServer = https.createServer(credentials, app);
+
+httpsServer.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
 });
